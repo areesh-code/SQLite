589d66e2ed2548ff8252d58924ded6a2a8d7d5e9
areesh-code <areeshdinani@yahoo.com>
2025-09-04 20:15:31 +1200

    Add files via upload

--- Activity1.sql
+++ Activity1.sql
@@ -1,15 +1,36 @@
-Create TABLE Supplier (
-    snumber INT PRIMARY KEY,
-    sname text,
-    sage INT,
-    scity text
+CREATE TABLE Saleman1 (
+SID INT PRIMARY KEY,
+SNAME TEXT,
+SCITY TEXT,
+SCOUNTRY TEXT
 );
 
-INSERT INTO Supplier (snumber, sname, sage, scity) VALUES
-(1, 'Julian', 13, 'New York'),
-(2, 'Cade', 12, 'Auckand'),
-(3, 'Juily', 8, 'Christchurch'),
-(4, 'Mathew', 6, 'Wellington'),
-(5, 'Freddie', 40, 'Queenstown');
+INSERT INTO Saleman1 (SID, SNAME, SCITY, SCOUNTRY) VALUES
+(191734, 'Areesh Dinani', 'Atlanta', 'USA'),
+(191326, 'Matthew Crean', 'Mumbai', 'India'),
+(191017, 'Julian Guan', 'Christchurch', 'NZ'),
+(191234, 'Cade Johnson', 'Dunedin', 'NZ'),
+(191737, 'Freddie Walker', 'Welington', 'NZ');
 
-select * from Supplier;
+CREATE TABLE Customers1 (
+CID INT PRIMARY KEY,
+CNAME TEXT,
+CCITY TEXT,
+SID INT
+);
+
+INSERT INTO Customers1 (CID, CNAME, CCITY, SID) VALUES
+(101, 'John Smith', 'Atlanta', 182345),
+(102, 'Rahul Dravid', 'New Delhi', 191326),
+(103, 'Brendon McCullum', 'Christchurch', 234738),
+(104, 'Tom Latham', 'Otago', 191284),
+(105, 'Martin Guptill', 'Auckland', 191234);
+
+SELECT C.CNAME, C.CCITY, S.SNAME FROM Customers1 C
+JOIN Saleman1 S ON C.SID = S.SID;
+
+SELECT C.CNAME, C.CCITY, S.SNAME FROM Customers1 C
+JOIN Saleman1 S ON C.CCity = S.SCity;
+
+
+
